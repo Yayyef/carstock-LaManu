bb7e4ba626df5c396a04a3719bc04cf3d2285f8f
Xavier Chassang <chassang.xavier@gmail.com>
2022-02-11 11:45:34 +0100

    First commit

---
+++ Carstock/Carstock/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carstock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
