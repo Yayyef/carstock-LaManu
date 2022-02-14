f741573d65566e0152ea08783feefc185bf1fc28
Xavier Chassang <chassang.xavier@gmail.com>
2022-02-14 11:45:22 +0100

    Combined queries and search tags added.

--- Carstock/Carstock/Controllers/ClientController.cs
+++ Carstock/Carstock/Controllers/ClientController.cs
@@ -21,22 +21,6 @@ namespace Carstock.Controllers
 
         public IActionResult Catalogue(string category, string brand, string type)
         {
-            // Comment faire la même chose avec la syntaxe avec des Where?
-            var carModels = from c in _context.Carmodels select c;
-
-            if (!String.IsNullOrEmpty(category))
-            {
-                carModels = carModels.Where(b => b.Category == category);
-            }
-            if (!String.IsNullOrEmpty(brand))
-            {
-                carModels = carModels.Where(b => b.Brand == brand);
-            }
-            if (!String.IsNullOrEmpty(type))
-            {
-                carModels = carModels.Where(b => b.Type == type);
-            }
-            
             // Je récupère les marques de voitures avec Select() mais seulement lorsqu'elles sont distinctes avec Distinct()
             var brands = _context.Carmodels.Select(b => b.Brand).Distinct();
             var types = _context.Carmodels.Select(t => t.Type).Distinct();
@@ -45,6 +29,27 @@ namespace Carstock.Controllers
             ViewBag.Brands = brands;
             ViewBag.Types = types;
             ViewBag.Categories = categories;
+
+            // Comment faire la même chose avec la syntaxe avec des Where?
+            var carModels = from c in _context.Carmodels select c;
+
+            if (!String.IsNullOrEmpty(brand))
+            {
+                carModels = carModels.Where(b => b.Brand == brand);
+                ViewData["selectedBrand"] = brand;
+            }
+            if (!String.IsNullOrEmpty(type))
+            {
+                carModels = carModels.Where(b => b.Type == type);
+                ViewData["selectedType"] = type;
+            }
+            if (!String.IsNullOrEmpty(category))
+            {
+                carModels = carModels.Where(b => b.Category == category);
+                ViewData["selectedCategory"] = category;
+            }
+
+           
 
             return View(carModels);
         }
