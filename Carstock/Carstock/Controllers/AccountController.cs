c940acfe06364ffd5e12d5e52558604a46bd19b1
mohamedommran <mohamed.ommran@gmail.com>
2022-02-17 09:52:27 +0100

    adding account connections

---
+++ Carstock/Carstock/Controllers/AccountController.cs
@@ -0,0 +1,120 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Carstock.Data;
+using Carstock.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
+namespace Carstock.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly carstockContext _context;
+
+        public AccountController(carstockContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Account
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Customers.ToListAsync());
+        //}
+        // GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("IdCustomer,Firstname,Lastname,Password,BankAccount,Email")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Login(LoginViewModel objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (carstockContext db = new carstockContext())
+                {
+
+                    var obj_user = db.Customers.Where(a => a.Email.Equals(objUser.Email) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    var obj_admin = db.Admins.Where(a => a.Email.Equals(objUser.Email) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj_user != null)
+                    {
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name,objUser.Email)
+                        };
+                        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        var principal = new ClaimsPrincipal(identity);
+                        var props = new AuthenticationProperties();
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, props).Wait();
+                        return RedirectToAction("Index", "Account");
+                    }
+                    else if (obj_admin != null)
+                    {
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name,objUser.Email)
+                        };
+                        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        var principal = new ClaimsPrincipal(identity);
+                        var props = new AuthenticationProperties();
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, props).Wait();
+
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else
+                    {
+                        //affiche un message login failed try again
+                        return View();
+                    }
+                }
+            }
+            else
+            {
+                return View(objUser);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
+
